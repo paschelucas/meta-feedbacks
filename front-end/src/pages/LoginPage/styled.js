ce046c60cac3e00631645ba1baff998bd61e223e
Adriano Brito <adrianobrito3@hotmail.com>
2022-05-24 17:26:45 -0300

    estilizando págia de login

---
+++ front-end/src/pages/LoginPage/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ContainerLogin = styled.div`
+display:flex;
+justify-content:center;
+flex-direction:column;
+align-items:center;
+margin-top:40vh;
+`
