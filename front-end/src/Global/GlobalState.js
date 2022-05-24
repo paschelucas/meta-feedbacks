dc6353c2e9f5ae1704022572f09375b77c3ec24c
Leona Christen <leo.christenmacedo@gmail.com>
2022-05-24 16:33:15 -0300

    Criando função de Login e integração de lib use form

---
+++ front-end/src/Global/GlobalState.js
@@ -0,0 +1,36 @@
+import GlobalContext from "./GlobalContext";
+import useRequest from '../hooks/useRequest.js';
+import tokenMock from "../mocks/tokenMock";
+import { goToHome } from "../routes/coordinator";
+import { useNavigate } from "react-router-dom";
+
+const GlobalState = (props) => {
+
+    const {makeRequest, isLoading} = useRequest();
+    const navigate = useNavigate();
+
+    // simulação do comportamento do login
+    const login = (data) => {
+            const token = tokenMock;  
+
+            console.log(data);
+
+            if (token) {
+                localStorage.setItem("token", token);
+            }
+            else {
+                alert('Deu ruim');
+            }
+
+            goToHome(navigate)
+    }; 
+
+    const value = {login, isLoading};
+
+    return (
+        <GlobalContext.Provider value={value}>
+            {props.children}
+        </GlobalContext.Provider>
+    );
+};
+export default GlobalState;
