882a0c0d29224dcd4329e3e4422f6e92c91b99c7
Adriano Brito <adrianobrito3@hotmail.com>
2022-05-25 08:14:37 -0300

    estilizando

--- front-end/src/pages/LoginPage/styled.js
+++ front-end/src/pages/LoginPage/styled.js
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 
+
+
 export const ContainerLogin = styled.div`
+width: 200px;
 display:flex;
 justify-content:center;
 flex-direction:column;
 align-items:center;
-margin-top:40vh;
+margin-top:10vh;
+padding: 40px 40px; 
+border:solid black 2px;
+border-radius: 8px;
+background-color: #ffff;
+`
+export const Input = styled.input`
+border-radius:5px;
+padding: 10px;
 `
