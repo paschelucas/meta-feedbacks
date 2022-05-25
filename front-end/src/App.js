e18aaf32b1b2f1dec4383d971c8d56e8b1925df4
Adriano Brito <adrianobrito3@hotmail.com>
2022-05-25 18:24:17 -0300

    pagina de login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,23 @@
 import Router from "./routes/Router";
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    
+  }
+
+  p {
+    margin: 0;
+  }
+  `
+
 
 const App = () => {
   return (
     <div>
+        <GlobalStyle/>
         <Router/>
     </div>
   );
--- front-end/src/pages/LoginPage/LoginPage.js
+++ front-end/src/pages/LoginPage/LoginPage.js
@@ -1,28 +1,30 @@
 import React, { useContext } from "react"; 
 import { useForm } from "react-hook-form";
 import GlobalContext from '../../Global/GlobalContext.js'
-import { ContainerLogin,Input} from "./styled.js";
-
-
+import { ContainerLogin,Input,Img,H3,Button,Span} from "./styled.js";
+import background from '../../assets/img/background.png'
+import logo from '../../assets/img/logo.png'
 
 const LoginPage = () => {
   const {register, handleSubmit, formState: {errors}} = useForm();
   const {login} = useContext(GlobalContext);
 
   return (
+    <div>
+      <Img src={background}/>
     <ContainerLogin>
-    
-      <h3><strong>Bem vindo !</strong></h3>
-      faça o seu login
+      <img src={logo}/>
+      <H3><strong>Bem vindo !</strong></H3>
+      <Span>faça o seu login</Span>
       <form onSubmit={handleSubmit(login)}>
         <Input {...register("name", {required: "precisa ter um nome"})} type="text" placeholder="digite seu login"/>
         {errors ? <p>{errors.name?.message}</p> : <></>}
         <Input {...register("password", {required: true})} type="text" placeholder="digite sua senha"/>
         {errors ? <p>{errors.password?.message}</p> : <></>}
-        <button type="submit">Entrar</button>
+        <Button type="submit">ENTRAR</Button>
       </form>
-     
-    </ContainerLogin>
+     </ContainerLogin>
+    </div>
   )
 }
 
