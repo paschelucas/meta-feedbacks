677213c3d2c0571727e7d413e0285356c1ca3c48
Lucas Pasche <paschelucas9@gmail.com>
2022-05-28 15:01:52 -0300

    [feat] endpoint de excluir formulário feito

--- back-end/src/business/FormBusiness.ts
+++ back-end/src/business/FormBusiness.ts
@@ -1,10 +1,11 @@
 import { FormDatabase } from "../data/FormDatabase";
+import { FormsAndItsQuestionsDatabase } from "../data/FormsAndItsQuestionsDatabase";
 import { CustomError } from "../error/CustomError";
 import { Form } from "../model/Form";
 import { FormDTO } from "../types/DTO/FormDTO";
 
 export class FormBusiness {
-  constructor(private formDatabase: FormDatabase) {}
+  constructor(private formDatabase: FormDatabase, private formsAndItsQuestionsDatabase: FormsAndItsQuestionsDatabase) {}
 
   public createForm = async (form: FormDTO) => {
     try {
@@ -33,10 +34,28 @@ export class FormBusiness {
     try {
       const forms = await this.formDatabase.getAllForms();
 
-      return forms
+      return forms;
     } catch (error: any) {
       throw new CustomError(error.statusCode, error.message);
-
     }
-  }
+  };
+
+  public deleteForm = async (formId: string): Promise<void> => {
+    try {
+      const foundForm = await this.formDatabase.getFormById(formId);
+      if (!foundForm) {
+        throw new CustomError(422, "Formulário não encontrado.");
+      }
+
+      const selectedFormQuestions = await this.formsAndItsQuestionsDatabase.getQuestionsByFormId(formId);
+
+      for (let question of selectedFormQuestions) {
+        await this.formsAndItsQuestionsDatabase.removeQuestionFromAForm(formId, question.question_id)
+      }
+
+      await this.formDatabase.deleteForm(formId)
+    } catch (error: any) {
+      throw new CustomError(error.statusCode, error.message);
+    }
+  };
 }
