1dacaba50e291cab92cd7113148f16e2d40c6e72
Leona Christen Macedo <leo.christenmacedo@gmail.com>
2022-06-04 15:36:54 -0300

    Criando opções referentes a projetos

--- front-end/src/pages/LeaguerProfilePage/LeaguerProfilePage.js
+++ front-end/src/pages/LeaguerProfilePage/LeaguerProfilePage.js
@@ -1,12 +1,22 @@
 import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import GlobalContext from "../../Global/GlobalContext";
 import { goBack } from "../../routes/coordinator";
+import Popup from "reactjs-popup";
+import GlobalContext from "../../Global/GlobalContext";
 
 const LeaguerProfilePage = () => {
     const navigate = useNavigate();
+    const {addUserInProject, projects} = useContext(GlobalContext);
+    const {handleSubmit, register} = useForm();
     const leaguerStr = localStorage.getItem('leaguer');
     const leaguer = JSON.parse(leaguerStr);
+
+    const projectDropDown = projects.map((project) => {
+        return (
+            <option key={project.project_id} value={project.project_id}>{project.project_name}</option>
+        );
+    });
 
     return (
         <>
@@ -18,7 +28,17 @@ const LeaguerProfilePage = () => {
             <p>Turma: {leaguer.leaguer_turma}</p>
             <p>Fase: {leaguer.leaguer_fase}</p>
             <p>Responsável: {leaguer.leaguer_responsavel}</p>
-            <button type="button">Histórico de projetos</button>
+            <Popup trigger={<button type="button">Adicionar a um projeto</button>} position="center">
+                {close => (
+                <form onSubmit={handleSubmit((data)=> addUserInProject(data, leaguer.leaguer_id))}>
+                    <select {...register('project')}>
+                        <option value="">Selecione</option>
+                        {projectDropDown}
+                    </select>
+                    <button type='submit'>Adicionar</button>
+                </form>
+                )}
+            </Popup>
         </>
     );
 };
